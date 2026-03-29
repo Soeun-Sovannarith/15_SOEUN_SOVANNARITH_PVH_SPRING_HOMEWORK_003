3b729bca8f8b82a8eb968e04fa46b4649082cfc2
Soeun-Sovannarith <soeunsovannarith64@gmail.com>
2026-03-29 20:53:32 +0700

    handling the time exception

--- test-api-docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.arguments="--server.port=8089" &
-PID=$!
-sleep 15
-curl -s http://localhost:8089/v3/api-docs > api-docs.json
-kill $PID
